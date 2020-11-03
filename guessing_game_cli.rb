520ac55f928e461fc21c48da68059ec077dd49f6
Ben Swanson <bswan0002@gmail.com>
2020-11-03 00:50:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,8 @@
 # Code your solution here!
 def run_guessing_game
   correctNum = rand(6) + 1
-  #puts "Guess a number 1 through 6, or type 'exit'"
   userGuess = gets.chomp
+  
   if userGuess == 'exit'
     puts "Goodbye!"
   elsif userGuess.to_i == correctNum
